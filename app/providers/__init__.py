b9ca6e90830939d90b2fb3da78d3f796c632e848
Noaman-Akhtar <akhtarnoaman@gmail.com>
2026-06-24 23:45:02 +0530

    feat: add BaseProvider interface and GenerationParams dataclassDefines the abstract contract that all model providers must implement.RAGAgent will depend on this interface instead of HuggingFace directly.Also fixes .gitignore to properly ignore the venv/ directory.

---
+++ app/providers/__init__.py
@@ -0,0 +1,24 @@
+# Copyright (C) 2026 Sugar Labs, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+"""
+Provider package for Sugar-AI.
+
+Providers are the abstraction layer between RAGAgent and model backends.
+"""
+from app.providers.base import BaseProvider, GenerationParams
+
+__all__ = ["BaseProvider", "GenerationParams"]
